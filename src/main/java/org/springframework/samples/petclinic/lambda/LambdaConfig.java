84b22b7c380dda270e8b1702275fed6687f824b1
Ben Foster <ben.foster@infinityworks.com>
2021-07-18 08:47:09 +0100

    Fixed lambda function signature that returns a correctly formed response back to ALB

--- src/main/java/org/springframework/samples/petclinic/lambda/LambdaConfig.java
+++ src/main/java/org/springframework/samples/petclinic/lambda/LambdaConfig.java
@@ -1,31 +1,46 @@
 package org.springframework.samples.petclinic.lambda;
 
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 @Configuration
 public class LambdaConfig {
 
     private static final Logger LOG = LoggerFactory.getLogger(LambdaConfig.class);
 
-    @Autowired
     private ClinicService clinicService;
 
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public LambdaConfig(final ClinicService clinicService, final ObjectMapper objectMapper) {
+        this.clinicService = clinicService;
+        this.objectMapper = objectMapper;
+    }
+
     @Bean
-    public Supplier<Collection<Owner>> getAllOwners() {
-        return () -> {
+    public Function<APIGatewayProxyRequestEvent, Message<Collection<Owner>>> getAllOwners() {
+        return (requestEvent) -> {
             LOG.info("Lambda Request for all Owners");
-            return this.clinicService.findAllOwners();
-		};
+            final Collection<Owner> allOwners = this.clinicService.findAllOwners();
+            final Map<String, Object> headers = new HashMap<>();
+            headers.put("Content-Type", "application/json");
+            return new GenericMessage<>(allOwners, headers);
+        };
     }
 
     @Bean
